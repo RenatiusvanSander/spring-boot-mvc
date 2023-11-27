274503f24f644a8b708fc1841724ee70a55780b6
Remy Meier <remad@web.de>
2023-11-27 13:21:28 +0100

    master: show model values in home view. Model is project

--- src/main/java/edu/remad/bootmvc/controllers/MainController.java
+++ src/main/java/edu/remad/bootmvc/controllers/MainController.java
@@ -1,14 +1,25 @@
 package edu.remad.bootmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.remad.bootmvc.data.entities.Project;
 
 @Controller
 @RequestMapping("/home")
 public class MainController {
 
 	@RequestMapping("/")
-	public String greeting() {
+	public String greeting(Model model) {
+		
+		Project project = new Project();
+		project.setName("First project name");
+		project.setSponsor("Nasa");
+		project.setDescription("This is a simple project sponsored by Nasa");
+		
+		model.addAttribute("currentProject", project);
+		
 		return "home";
 	}
 }
